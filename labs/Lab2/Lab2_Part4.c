c8117ed4a7c5d72ca703651904d00874fde9df79
sadhikahuria <127627132+sadhikahuria@users.noreply.github.com>
2025-05-21 12:39:31 -0700

    lab2 completed

--- labs/Lab2/Lab2_Part4.c
+++ labs/Lab2/Lab2_Part4.c
@@ -31,14 +31,33 @@ void printBinaryWithPadding(int num) {
 int main(int argc, char *argv[]) {
 
   int x = 0xDECAF000;  // "0x" means hexadecimal number
+  // 32 bit = 4 bytes
   short y = 0xCAFE;
+  // 16 bit = 2 bytes
   char z = y;
+  // 8 bit = 1 byte
 
-  if ( x > y ) printf("Cafe\n");
+  // x = DECA F000 -> negative 
+  // y = CAFE -> negative
+  // x = FE -> negative
 
-  if ( x < z ) printf("Decaf\n");
+  printf("x = %d\ny = %d\nx = %d\n\n", x, y, z);
 
-  if ( z > y ) printf("Latte\n");  
+  printf("x as unsigned: %u\nx as singed: %d\n", (unsigned int)x, (signed int)x);
+  printf("y as unsigned: %u\ny as singed: %d\n", (unsigned int)y, (signed int)y);
+  printf("z as unsigned: %u\nz as singed: %d\n\n", (unsigned int)z, (signed int)z);
+
+
+
+  if ( (short)x > y ) printf("Cafe\n"); 
+
+  // comparing int and short, short is promoted to an int
+
+  if ( (char)x < z ) printf("Decaf\n");
+  // comparing int and char, char is compared as it
+
+  if ( z > (unsigned short)y ) printf("Latte\n");  
+  // comparing char and short, char is promoted to int, short is promoted to int
 
   return 0;
 }
