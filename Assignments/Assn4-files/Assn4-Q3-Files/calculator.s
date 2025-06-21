b0266ef7cc0123def1cad24076d454eaab2f3df5
sadhikahuria <127627132+sadhikahuria@users.noreply.github.com>
2025-06-20 22:32:44 -0700

    a4 q3 done

--- Assignments/Assn4-files/Assn4-Q3-Files/calculator.s
+++ Assignments/Assn4-files/Assn4-Q3-Files/calculator.s
@@ -1,17 +1,24 @@
-# add a header comment block
+# Name: Sadhika Huria
+# Student id: 301599274 
+# Date June 20th 2025
+# calculator.s, assembly code for a calculator
 
-	.globl	XX # Make sure you change the name of this function - see XX function below
+	.globl	is_less		# Make sure you change the name of this function - see XX function below
 	.globl	plus
 	.globl	minus
 	.globl	mul
 
 
-XX: # Description: 
+is_less: # Description: 
     # Change the name of this function to something more descriptive and add a description above
-	xorl	%eax, %eax
-	cmpl	%esi, %edi
-	setl	%al         # See Section 3.6.2 of our textbook for a  
-	ret                 # description of the set* instruction family
+
+	# edi -> x, esi -> y
+	xorl	%eax, %eax	# return value set to 0
+	cmpl	%esi, %edi	# x - y < 0 s.t. x < y, then setl is executed. 
+	setl	%al         # if x < y, return value is set to 1
+						# See Section 3.6.2 of our textbook for a  
+	ret                 # returns 1, if x < y, otherwise 0
+						# description of the set* instruction family
 
 plus: # Description: Performs integer addition
 # Requirement:
@@ -19,7 +26,12 @@ plus: # Description: Performs integer addition
 # - you cannot use a loop
 
 # Put your code here
+# edi -> x, esi -> y
 
+    movl    %edi, %eax      # set return value to x
+    negl    %esi            # change y to -y 
+    subl    %esi, %eax      # set return val to x-(-y) (so, equivalent to x+y)
+	ret
 
 minus: # Description: Performs integer subtraction
 # Requirement:
@@ -27,7 +39,12 @@ minus: # Description: Performs integer subtraction
 # - you cannot use a loop
 
 # Put your code here
+# edi -> x, esi -> y
 
+    movl    %edi, %eax      # set return value to x
+    negl    %esi            # change y to -y 
+    addl    %esi, %eax      # set return val to x+(-y) (so, equivalent to x-y)
+	ret
 
 mul: # Description: Performs integer multiplication - when both operands are non-negative!
 # You can assume that both operands are non-negative.
@@ -36,10 +53,39 @@ mul: # Description: Performs integer multiplication - when both operands are non
 #   (or any kind of instruction that multiplies such as mul)
 # - you must use a loop
 
+
 # algorithm:
-#
-#
+# result = 0
+# while (y != 0){
+#	if( y is odd ){
+#		result += x;
+#	}
+#	x = x * 2;
+#	y = y / 2;	
+# }
 #
 #
 
 # Put your code here
+# edi -> x, esi -> y
+	xorl	%eax , %eax		# set result to 0
+
+	loop:
+	testl  	%rsi, %rsi 		# if rsi, y is 0, end loop
+	je endloop
+
+	testl $1, %esi 			#check if esi is even or odd, comparing checking LSb
+
+	jz dont_add 			# if even, dont add x to result
+	addl %edi, %eax			
+
+	
+	dont_add:
+		shll $1, edi
+		shrl $1, esi
+		jmp loop
+
+
+	endloop:
+
+	ret
--- Assignments/Assn4-files/Assn4-Q3-Files/main.c
+++ Assignments/Assn4-files/Assn4-Q3-Files/main.c
@@ -11,7 +11,8 @@
 #include <stdlib.h>  // atoi()
 #include <stdio.h>   // printf()
 
-int XX(int x, int y);  // Make sure you change the name of this function - see calculator.s
+int is_less(int x, int y);  // Make sure you change the name of this function - see calculator.s
+// returns 1 if x is less than y, else 0
 int plus(int x, int y);
 int minus(int x, int y);
 int mul(int x, int y);
@@ -26,7 +27,7 @@ int main(int argc, char *argv[]) {
     x = atoi(argv[1]);
     y = atoi(argv[2]);
 
-    result = XX(x, y); // Make sure you change the name of this function - see calculator.s
+    result = is_less(x, y); // Make sure you change the name of this function - see calculator.s
     printf("%d ??? %d -> %d\n", x, y, result); // Make sure you change ??? to the appropriate symbol
 
     result = plus(x, y);
