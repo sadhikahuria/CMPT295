8f130cabb746a9be0c07c1fec9bd53a6542abc7f
sadhikahuria <sadhikahuria@gmail.com>
2025-07-03 20:23:10 -0700

    added files for a5

---
+++ Assignments/Assn5-files/main.s
@@ -0,0 +1,26 @@
+main:
+    xorq    %rax, %rax
+    xorl    %eax, %eax
+    movq    $0, %rax
+    subl    %eax, %eax
+    imull   $0, %eax
+    andl    $0, %eax
+
+
+    addl    $1, %eax
+    leal    1(%eax), %eax
+    incl    %eax
+    subl    $-1, %eax
+
+    addl    $8, %eax
+    leal    8(%eax), %eax
+
+    subq    $8, %rsp
+    movq    %rdi, (%rsp)
+    pushq   %rdi
+
+    movq    (%rsp), %rsi
+    addq    $8, %rsp
+    popq    %rsi
+
+    ret
