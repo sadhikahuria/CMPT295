25a7dcdba84d1637c131eae631f95ba7a38df443
sadhika.huria <sadhikahuria@gmail.com>
2025-07-03 22:04:03 -0700

    a5q1 done

--- Assignments/Assn5-files/main.s
+++ Assignments/Assn5-files/main.s
@@ -1,26 +1,25 @@
-main:
-    xorq    %rax, %rax
-    xorl    %eax, %eax
-    movq    $0, %rax
-    subl    %eax, %eax
-    imull   $0, %eax
-    andl    $0, %eax
+xorq    %rax, %rax
+xorl    %eax, %eax
+movq    $0, %rax
+movl    $0, %eax
+subl    %eax, %eax
+imull   $0, %eax
+andl    $0, %eax
 
 
-    addl    $1, %eax
-    leal    1(%eax), %eax
-    incl    %eax
-    subl    $-1, %eax
+addl    $1, %eax
+leal    1(%eax), %eax
+incl    %eax
+subl    $-1, %eax
 
-    addl    $8, %eax
-    leal    8(%eax), %eax
+addl    $8, %eax
+leal    8(%eax), %eax
 
-    subq    $8, %rsp
-    movq    %rdi, (%rsp)
-    pushq   %rdi
+subq    $8, %rsp
+movq    %rdi, (%rsp)
+pushq   %rdi
 
-    movq    (%rsp), %rsi
-    addq    $8, %rsp
-    popq    %rsi
+movq    (%rsp), %rsi
+addq    $8, %rsp
+popq    %rsi
 
-    ret
